a0dc99fb0dd532c090d854f1ea3d4ab5aaf389ef
vrait35 <vrait35@mail.ru>
2018-09-17 02:06:26 +0600

    dz_indexer

---
+++ Indexer/MyArray.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexer
+{
+    public class MyArray
+    {                
+        public int[] Array { get; set; }
+        public int this[int index]
+        {
+            get {
+                try
+                {
+                    return Array[index];
+                }
+                catch
+                {
+                    throw new Exception("Вы пытаетесь взять значения у переменной которой не существует!");
+                }
+            }
+            set
+            {
+                try
+                {
+                    Array[index] = value * value;
+                }
+                catch {
+                    throw new Exception("Вы пытаетесь присвоить значения не существующей переменной!");
+                }
+            }
+        }
+        public MyArray()
+        {
+            Array = new int[1000];
+        }
+        public MyArray(int size)
+        {
+            Array = new int[size];
+        }
+    }
+}
---
+++ Indexer/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyArray myArray = new MyArray();
+            myArray[5] = 7;
+            Console.WriteLine("myArray[5]="+myArray[5]);
+            MyArray myArray2 = new MyArray(7);
+            try
+            {
+                myArray2[9] = 6;
+            Console.WriteLine("myArray2[9]=" + myArray2[9]);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
